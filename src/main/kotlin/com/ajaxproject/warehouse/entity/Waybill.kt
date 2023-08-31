b3201a6ee8f099a56a159ca87d673d0f662f4450
Dmytro Onopriienko <dmitry.onopriienko@gmail.com>
2023-08-31 16:55:49 +0300

    Remove unused imports

--- src/main/kotlin/com/ajaxproject/warehouse/entity/Waybill.kt
+++ src/main/kotlin/com/ajaxproject/warehouse/entity/Waybill.kt
@@ -1,6 +1,5 @@
 package com.ajaxproject.warehouse.entity
 
-import com.fasterxml.jackson.annotation.JsonBackReference
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -18,7 +17,6 @@ class Waybill(
     var id: Int,
 
     @ManyToOne
-//    @JsonBackReference
     @JoinColumn(name = "customer_id")
     var customer: Customer,
     var date: Date,
--- src/main/kotlin/com/ajaxproject/warehouse/entity/WaybillProduct.kt
+++ src/main/kotlin/com/ajaxproject/warehouse/entity/WaybillProduct.kt
@@ -1,6 +1,5 @@
 package com.ajaxproject.warehouse.entity
 
-import com.fasterxml.jackson.annotation.JsonBackReference
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
@@ -13,7 +12,6 @@ class WaybillProduct(
     @Id
     @ManyToOne
     @JoinColumn(name = "waybill_id")
-//    @JsonBackReference
     var waybill: Waybill,
     @Id
     @ManyToOne
