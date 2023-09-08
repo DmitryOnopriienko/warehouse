0a8cbe4ee122eafe08bd52f89a4db367bf6729aa
Dmytro Onopriienko <dmitry.onopriienko@gmail.com>
2023-09-08 19:44:50 +0300

    Add dto for creating product, add creating method and endpoint, add validation

--- src/main/kotlin/com/ajaxproject/warehouse/service/ProductServiceImpl.kt
+++ src/main/kotlin/com/ajaxproject/warehouse/service/ProductServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.ajaxproject.warehouse.service
 
+import com.ajaxproject.warehouse.dto.ProductCreateDto
 import com.ajaxproject.warehouse.dto.ProductDataDto
 import com.ajaxproject.warehouse.dto.ProductDataLiteDto
 import com.ajaxproject.warehouse.entity.Product
@@ -19,6 +20,10 @@ class ProductServiceImpl(val productRepository: ProductRepository) : ProductServ
             .mapToDataDto()
     }
 
+    override fun createProduct(createDto: ProductCreateDto) {
+        productRepository.save(createDto.mapToEntity())
+    }
+
     fun Product.mapToLiteDto(): ProductDataLiteDto = ProductDataLiteDto(
         id = id,
         title = title,
@@ -28,6 +33,14 @@ class ProductServiceImpl(val productRepository: ProductRepository) : ProductServ
 
     fun Product.mapToDataDto(): ProductDataDto = ProductDataDto(
         id = id,
+        title = title,
+        price = price,
+        amount = amount,
+        about = about
+    )
+
+    fun ProductCreateDto.mapToEntity(): Product = Product(
+        id = null,
         title = title,
         price = price,
         amount = amount,
