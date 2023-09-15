527de701056db3a84b58b2fae91789ba56fa7d4d
Dmytro Onopriienko <dmitry.onopriienko@gmail.com>
2023-09-15 12:31:11 +0300

    Rename annotation

--- src/main/kotlin/com/ajaxproject/warehouse/annotation/RateLimit.kt
+++ src/main/kotlin/com/ajaxproject/warehouse/annotation/RateLimit.kt
@@ -2,4 +2,4 @@ package com.ajaxproject.warehouse.annotation
 
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME )
-annotation class RequestLimit   // TODO make limit settable, smth like: (val value: Int = 10)
+annotation class RateLimit(val value: Int = 10)   // TODO make limit settable, smth like: (val value: Int = 10)
