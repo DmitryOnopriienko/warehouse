c10b52eccb8a3002522974c7a494fe6835cdd32f
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-09-17 18:01:09 +0300

    Remove unnecessary serialVersionUID

--- src/main/kotlin/com/ajaxproject/warehouse/exception/MethodRateLimitExceededException.kt
+++ src/main/kotlin/com/ajaxproject/warehouse/exception/MethodRateLimitExceededException.kt
@@ -1,10 +1,3 @@
 package com.ajaxproject.warehouse.exception
 
-import java.io.Serial
-
-class MethodRateLimitExceededException(message: String?) : RuntimeException(message) {
-    companion object {
-        @Serial
-        private const val serialVersionUID: Long = 106759828112291430L
-    }
-}
+class MethodRateLimitExceededException(message: String?) : RuntimeException(message)
