9165f2b5599a1e51baff4029d5dd3a80ba4ce51b
Dmytro Onopriienko <dmitry.onopriienko@gmail.com>
2023-10-03 18:02:55 +0300

    Remove SVUID

--- src/main/kotlin/com/ajaxproject/warehouse/exception/NotFoundException.kt
+++ src/main/kotlin/com/ajaxproject/warehouse/exception/NotFoundException.kt
@@ -1,16 +1,9 @@
 package com.ajaxproject.warehouse.exception
 
-import java.io.Serial
-
 class NotFoundException(message: String?) : RuntimeException(message) {
     constructor(errorList: List<String>) : this(null) {
         this.errorList = errorList
     }
 
     var errorList: List<String>? = null
-
-    companion object {
-        @Serial
-        private const val serialVersionUID: Long = 7934118737327701468L
-    }
 }
