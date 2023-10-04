a43314994daa0c25e35cdeb25ff64e989bcf47ff
Dmytro Onopriienko <dmitry.onopriienko@gmail.com>
2023-10-04 16:10:55 +0300

    Remove unused imports

--- wm-system/src/main/kotlin/com/ajaxproject/warehouse/config/NatsConfig.kt
+++ wm-system/src/main/kotlin/com/ajaxproject/warehouse/config/NatsConfig.kt
@@ -1,12 +1,10 @@
 package com.ajaxproject.warehouse.config
 
 import io.nats.client.Connection
-import io.nats.client.Dispatcher
 import io.nats.client.Nats
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.nio.charset.StandardCharsets
 
 @Configuration
 class NatsConfig(
