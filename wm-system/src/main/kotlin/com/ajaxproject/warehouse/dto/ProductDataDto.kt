d106e7d1aebf4f1ea28d47d44f2ff341d83051be
Dmytro Onopriienko <dmitry.onopriienko@gmail.com>
2023-10-06 11:19:18 +0300

    Implement DTO mapToProto() methods for ProductData DTOs

--- wm-system/src/main/kotlin/com/ajaxproject/warehouse/dto/ProductDataDto.kt
+++ wm-system/src/main/kotlin/com/ajaxproject/warehouse/dto/ProductDataDto.kt
@@ -1,9 +1,19 @@
 package com.ajaxproject.warehouse.dto
 
+import com.ajaxproject.api.internal.warehousesvc.commonmodels.product.Product
+
 data class ProductDataDto(
     val id: String,
     val title: String,
     val price: Double,
     val amount: Int,
     val about: String?
-)
+) {
+    fun mapToProto(): Product = Product.newBuilder()
+        .setId(id)
+        .setTitle(title)
+        .setPrice(price)
+        .setAmount(amount)
+        .setAbout(about)
+        .build()
+}
--- wm-system/src/main/kotlin/com/ajaxproject/warehouse/dto/ProductDataLiteDto.kt
+++ wm-system/src/main/kotlin/com/ajaxproject/warehouse/dto/ProductDataLiteDto.kt
@@ -1,8 +1,17 @@
 package com.ajaxproject.warehouse.dto
 
+import com.ajaxproject.api.internal.warehousesvc.commonmodels.product.Product
+
 data class ProductDataLiteDto(
     val id: String,
     val title: String,
     val price: Double,
     val amount: Int
-)
+) {
+    fun mapToProto(): Product = Product.newBuilder()
+        .setId(id)
+        .setTitle(title)
+        .setPrice(price)
+        .setAmount(amount)
+        .build()
+}
