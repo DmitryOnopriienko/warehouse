8b8c9bda186ce821d3aaf9ee16a871f08f2be870
Dmytro Onopriienko <dmitry.onopriienko@gmail.com>
2023-10-06 11:23:00 +0300

    Implement FindAllProducts NATS controller

--- wm-system/src/main/kotlin/com/ajaxproject/warehouse/controller/nats/HelloWorldController.kt
+++ wm-system/src/main/kotlin/com/ajaxproject/warehouse/controller/nats/HelloWorldController.kt
@@ -1,20 +1,19 @@
-package com.ajaxproject.warehouse.nats
+package com.ajaxproject.warehouse.controller.nats
 
 import io.nats.client.Connection
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
 
 @Component
-class NatsController(
-    connection: Connection,
+class HelloWorldController(
+    connection: Connection
 ) {
-
-    val subj: String = "helloworld.get"
 
     init {
         connection.createDispatcher { msg ->
             val message = String(msg.data, StandardCharsets.UTF_8)
             println("Received message: $message")
-        }.subscribe(subj)
+            connection.publish("hi", msg.replyTo, "Hi $message".toByteArray())
+        }.subscribe("hw")
     }
 }
---
+++ wm-system/src/main/kotlin/com/ajaxproject/warehouse/controller/nats/product/FindAllProductsNatsController.kt
@@ -0,0 +1,38 @@
+package com.ajaxproject.warehouse.controller.nats.product
+
+import com.ajaxproject.api.internal.warehousesvc.NatsSubject.Product.FIND_ALL
+import com.ajaxproject.api.internal.warehousesvc.commonmodels.product.Product
+import com.ajaxproject.api.internal.warehousesvc.input.reqreply.product.FindAllProductsRequest
+import com.ajaxproject.api.internal.warehousesvc.input.reqreply.product.FindAllProductsResponse
+import com.ajaxproject.warehouse.controller.nats.NatsController
+import com.ajaxproject.warehouse.service.ProductService
+import com.google.protobuf.Parser
+import org.springframework.stereotype.Component
+
+@Component
+class FindAllProductsNatsController(
+    val productService: ProductService
+) : NatsController<FindAllProductsRequest, FindAllProductsResponse> {
+
+    override val subject: String = FIND_ALL
+
+    override val parser: Parser<FindAllProductsRequest> = FindAllProductsRequest.parser()
+
+    override fun handle(request: FindAllProductsRequest): FindAllProductsResponse {
+        runCatching {
+            return buildSuccessResponse(productService.findAllProducts().map { it.mapToProto() })
+        }.getOrElse { exception ->
+            return buildFailureResponse(exception)
+        }
+    }
+
+    fun buildSuccessResponse(products: List<Product>): FindAllProductsResponse =
+        FindAllProductsResponse.newBuilder().apply {
+            successBuilder.addAllProducts(products)
+        }.build()
+
+    fun buildFailureResponse(exception: Throwable): FindAllProductsResponse =
+        FindAllProductsResponse.newBuilder().apply {
+            failureBuilder.setMessage("Exception encountered: $exception")
+        }.build()
+}
